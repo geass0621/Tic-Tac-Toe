8c2d8dde1fc8f27677b4c0c8d48547737594f047
DongCuong <geas0621@gmail.com>
2025-05-01 16:37:33 +0300

    Added: Player component

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,20 @@
+import { Player } from "./components/Player/Player";
+import { useState } from "react";
 
 function App() {
-
+  const [isEditing, setIsEditing] = useState(false)
 
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-          <li>
-            <span className="player">
-              <span className="player-name">Player 1</span>
-              <span className="player-symbol">X</span>
-            </span>
-            <button>Edit</button>
-          </li>
-          <li>
-            <span className="player">
-              <span className="player-name">Player 2</span>
-              <span className="player-symbol">O</span>
-            </span>
-            <button>Edit</button>
-          </li>
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
         </ol>
         game board
       </div>
       log
-    </main>
+    </main >
   )
 }
 
